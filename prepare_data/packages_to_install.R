a1fb8098e8cb28f7152127aff4d6122f97930187
filippob <filippo.biscarini@gmail.com>
2019-09-03 18:51:56 +0200

    added the file get_data.sh (not a proper script so far)

---
+++ prepare_data/packages_to_install.R
@@ -0,0 +1,21 @@
+
+## general packages for data handling
+library("knitr")
+library("tidyr")
+library("dplyr")
+library("ggplot2")
+library("ggfortify")
+library("doParallel")
+library("data.table")
+
+## For the script gwas.r (GWAS with the kinship matrix)
+library("msm")
+library("doMC")
+library("nadiv")
+
+## own-made R package for G-matrix
+library("gMatrix")
+
+## GWAS and plots
+library("qqman")
+library("snpStats")
