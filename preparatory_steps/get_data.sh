f6bce7243c423238d7adaba19472e81e794d3707
filippob <filippo.biscarini@gmail.com>
2019-09-04 12:33:28 +0200

    EDA updated

--- preparatory_steps/get_data.sh
+++ preparatory_steps/get_data.sh
@@ -32,4 +32,4 @@ wget https://datadryad.org/bitstream/handle/10255/dryad.77585/UCD_2014.tped
 Rscript --vanilla prep_dogpheno.R UCD_2014.tfam
 
 ## subset genotypes
-~/Downloads/plink --dog --tfile UCD_2014 --chr 25,26,27,28,29 --recode transpose --out dogs
+~/Downloads/plink --dog --tfile UCD_2014 --chr 25,26,27,28,29 --recode --out dogs
