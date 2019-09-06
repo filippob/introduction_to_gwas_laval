b0afa075d6c15a1f433a182f109427a7efb39673
filippob <filippo.biscarini@gmail.com>
2019-09-06 11:53:02 +0200

    modified script prep_rice_data.R

--- preparatory_steps/prep_rice_data.R
+++ preparatory_steps/prep_rice_data.R
@@ -24,7 +24,7 @@ trait= args[3]
 print(paste("selected trait:",trait))
 
 
-setwd("~/Dropbox/cursos/laval2019/raw_data/")
+setwd("~/Dropbox/cursos/laval2019/data/")
 
 phenotypes = fread(phenotype_file)
 ref_group = fread(group_reference_file)
@@ -43,6 +43,9 @@ phenotypes$Accession[!(phenotypes$Accession %in% ref_group$id)]
 
 ## add population group to phenotype file
 phenotypes$population = ref_group$population[match(phenotypes$Accession, ref_group$id)]
+
+## rename column id (from Accession) 
+names(phenotypes)[1] <- "id"
 
 ## write out new phenotype file
 fwrite(x = phenotypes[ , c("Accession", "population", eval(target)), with=FALSE],
