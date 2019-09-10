0514e488f89e60bf8e2411c25887478f5ab6e20e
enormandeau <eric.normandeau.qc@gmail.com>
2019-09-10 16:55:10 +0000

    Number scripts and paths

--- 5.steps/steps.sh
+++ 5.steps/steps.sh
@@ -7,10 +7,10 @@
 ## filtering genotype data ##
 -----------------------------
 
-plink --file ~/Share/data/rice --geno 0.05 --mind 0.2 --maf 0.05 --recode --out rice_filtered
-~/Downloads/plink --file ~/Share/data/rice --geno 0.05 --mind 0.2 --maf 0.05 --recode --out rice_filtered
+plink --file ../data/rice --geno 0.05 --mind 0.2 --maf 0.05 --recode --out rice_filtered
+plink --file ../data/rice --geno 0.05 --mind 0.2 --maf 0.05 --recode --out rice_filtered
 
-plink --file ~/Share/data/dogs --dog --geno 0.05 --mind 0.2 --maf 0.05 --recode --out dogs_filtered
+plink --file ../data/dogs --dog --geno 0.05 --mind 0.2 --maf 0.05 --recode --out dogs_filtered
 
 --------------------------------------
 ## imputation of missing genotypes  ##
@@ -18,20 +18,18 @@ plink --file ~/Share/data/dogs --dog --geno 0.05 --mind 0.2 --maf 0.05 --recode 
 
 ## convert to vcf
 plink --file rice_filtered --recode vcf --out rice_filtered
-~/Downloads/plink --file rice_filtered --recode vcf --out rice_filtered
+plink --file rice_filtered --recode vcf --out rice_filtered
 
 plink --dog --file dogs_filtered --recode vcf --out dogs_filtered	## check the warning on underscores !!
 plink --dog --file dogs_filtered --recode vcf-iid --out dogs_filtered
 
 ## Beagle
-java -Xss5m -Xmx4g -jar ~/Documents/beagle4.1/beagle.27Jan18.7e1.jar gt=rice_filtered.vcf out=rice_imputed
 beagle gt=rice_filtered.vcf out=rice_imputed
-
 beagle gt=dogs_filtered.vcf out=dogs_imputed
 
 ## convert back to ped/map
 plink --vcf rice_imputed.vcf.gz --recode --out rice_imputed
-~/Downloads/plink  --vcf rice_imputed.vcf.gz --recode --out rice_imputed
+plink  --vcf rice_imputed.vcf.gz --recode --out rice_imputed
 
 plink --dog --vcf dogs_imputed.vcf.gz --recode --out dogs_imputed
 
@@ -41,12 +39,12 @@ plink --dog --vcf dogs_imputed.vcf.gz --recode --out dogs_imputed
 
 ## prepare data for gwas.R
 plink --file rice_imputed --recode A --out rice_imputed
-~/Downloads/plink --file rice_imputed --recode A --out rice_imputed
+plink --file rice_imputed --recode A --out rice_imputed
 
 plink --dog --file dogs_imputed --recode A --out dog_imputed
 
 
 ## GWAS
-Rscript --vanilla ../gwas/gwas.R genotype_file=rice_imputed.raw snp_map=rice_imputed.map phenotype_file=~/Share/data/rice_phenotypes.txt trait=PH trait_label=PH
-Rscript --vanilla ../gwas/gwas.R genotype_file=dogs_imputed.raw snp_map=dogs_imputed.map phenotype_file=~/Share/data/dogs_phenotypes.txt trait=phenotype trait_label=cleft_lip
+Rscript --vanilla ../4.gwas/gwas.R genotype_file=rice_imputed.raw snp_map=rice_imputed.map phenotype_file=../data/rice_phenotypes.txt trait=PH trait_label=PH
+Rscript --vanilla ../4.gwas/gwas.R genotype_file=dogs_imputed.raw snp_map=dogs_imputed.map phenotype_file=../data/dogs_phenotypes.txt trait=phenotype trait_label=cleft_lip
 
