30075ad0b69c9ef008f15b53f0555584dd3a2088
filippob <filippo.biscarini@gmail.com>
2019-09-11 02:53:03 +0200

    5.steps: seprate scripts for the different steps

---
+++ 5.steps/2.step_imputation.sh
@@ -0,0 +1,21 @@
+--------------------------------------
+## imputation of missing genotypes  ##
+--------------------------------------
+
+## convert to vcf
+plink --file rice_filtered --recode vcf --out rice_filtered
+plink --file rice_filtered --recode vcf --out rice_filtered
+
+plink --dog --file dogs_filtered --recode vcf --out dogs_filtered	## check the warning on underscores !!
+plink --dog --file dogs_filtered --recode vcf-iid --out dogs_filtered
+
+## Beagle
+beagle gt=rice_filtered.vcf out=rice_imputed
+beagle gt=dogs_filtered.vcf out=dogs_imputed
+
+## convert back to ped/map
+plink --vcf rice_imputed.vcf.gz --recode --out rice_imputed
+plink  --vcf rice_imputed.vcf.gz --recode --out rice_imputed
+
+plink --dog --vcf dogs_imputed.vcf.gz --recode --out dogs_imputed
+
