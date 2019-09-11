e917246800a1fa182599c0ffa261d41f2addd5b8
enormandeau <eric.normandeau.qc@gmail.com>
2019-09-11 20:46:43 +0000

    Update snakemake scripts

--- 6.pipeline/command_line.sh
+++ 6.pipeline/command_line.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ## prepare the env
 
+mkdir data 2>/dev/null
+mkdir steps 2>/dev/null
 cp -r ../software .
-cp ../gwas.R software/
-cp -r ../../data .
-mkdir steps
-
+cp ../4.gwas/gwas.R software
+cp -r ../data/{dogs,rice}.{ped,map} data
+cp -r ../data/{dogs,rice}_phenotypes.txt data
 
 ## dry run
 snakemake -n -s Snakefile_GWAS_continuous
