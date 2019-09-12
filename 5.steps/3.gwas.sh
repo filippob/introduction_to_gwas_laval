31deefdebe0a497652a8b8a3ed05c4656ce7bc1c
enormandeau <eric.normandeau.qc@gmail.com>
2019-09-12 13:19:50 +0000

    Update for Thursday

--- 5.steps/3.gwas.sh
+++ 5.steps/3.gwas.sh
@@ -10,4 +10,3 @@ plink --dog --file dogs_imputed --recode A --out dog_imputed
 ## GWAS
 Rscript --vanilla ../4.gwas/gwas.R genotype_file=rice_imputed.raw snp_map=rice_imputed.map phenotype_file=../data/rice_phenotypes.txt trait=PH trait_label=PH
 Rscript --vanilla ../4.gwas/gwas.R genotype_file=dogs_imputed.raw snp_map=dogs_imputed.map phenotype_file=../data/dogs_phenotypes.txt trait=phenotype trait_label=cleft_lip
-
