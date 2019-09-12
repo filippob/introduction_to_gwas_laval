40416d7c3632a11cb1bafad055c0c63889b870cf
filippob <filippo.biscarini@gmail.com>
2019-09-12 20:24:18 +0200

    prepare parus data

--- 6.pipeline/command_line.sh
+++ 6.pipeline/command_line.sh
@@ -5,8 +5,8 @@ mkdir data 2>/dev/null
 mkdir steps 2>/dev/null
 cp -r ../software .
 cp ../4.gwas/gwas.R software
-cp -r ../data/{dogs,rice}.{ped,map} data
-cp -r ../data/{dogs,rice}_phenotypes.txt data
+cp -r ../data/{dogs,rice,parus}.{ped,map} data
+cp -r ../data/{dogs,rice,parus}_phenotypes.txt data
 
 ## dry run
-snakemake -n -s Snakefile_GWAS_continuous
+snakemake -s Snakefile_GWAS_parus
